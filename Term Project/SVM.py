f27249f858c090dde8eb835106ac1e47c2c855f4
Omar Khashoggi <omar.w.kh@gmail.com>
2017-12-14 16:39:12 +0300

    use relaxed metric in SVM

--- Term Project/SVM.py
+++ Term Project/SVM.py
@@ -5,12 +5,14 @@ from sklearn import metrics
 import random
 from sklearn.model_selection import KFold
 from sklearn.metrics import accuracy_score
+from our_metrics import relaxed_accuracy, print_performance
 import data
 
 #Functions Segment
 def use_kfolds(X,Y,n_splits,svm):
     kf = KFold(n_splits=n_splits, random_state=None, shuffle=False)
     accuracy = []
+    relaxed = []
     indices = []
     for train_in, test_in in kf.split(X):
         X_train = X[train_in]
@@ -20,8 +22,9 @@ def use_kfolds(X,Y,n_splits,svm):
         svm.fit(X_train, Y_train)
         y_hat = svm.predict(X_vald)
         accuracy.append(accuracy_score(Y_vald, y_hat))
+        relaxed.append(relaxed_accuracy(Y_vald, y_hat))
         indices.append([[[train_in[0],train_in[-1]],[test_in[0],test_in[-1]]]])
-    return accuracy,indices
+    return accuracy,relaxed,indices
 
 def use_SVC_reg(X,Y,percentage,svc):
     X_train = X[:int(len(X)*percentage)]
@@ -30,7 +33,7 @@ def use_SVC_reg(X,Y,percentage,svc):
     Y_vald = Y[int(len(Y)*percentage):]
     svc.fit(X_train,Y_train)
     y_hat = svc.predict(X_vald)
-    return accuracy_score(Y_vald, y_hat)
+    return Y_vald, y_hat
 
 def get_class_weights(df_Y):
     labels = np.unique(df_Y)
@@ -54,12 +57,14 @@ X, Y, X_test = data.get(fill_na=True, verbose=True) #, normalize_X=True)
 svc = SVC(decision_function_shape='ovr', kernel='rbf', C=2)
 
 percentage = .9
-reg_acc = use_SVC_reg(X,Y,percentage,svc)
-print 'Accuracy at {0:}% Seperation: {1:.2f}\n'.format(percentage*100, reg_acc)
+Y_vald, y_hat = use_SVC_reg(X,Y,percentage,svc)
+print_performance('SVC Accuracy at {0:}% Seperation PE'.format(percentage*100), Y_vald, y_hat)
+
 folds = 10
 print 'Using {}-Folds'.format(folds)
-kfold_acc, kfold_ind = use_kfolds(X,Y,folds,svc)
+kfold_acc, kfold_relaxed,kfold_ind = use_kfolds(X,Y,folds,svc)
 print 'KFolds Accuracies: {0:},\nhighest: {1:.2f} - {2:}'.format(kfold_acc,kfold_acc[np.argmax(kfold_acc)], kfold_ind[np.argmax(kfold_acc)])
+print 'KFolds Relaxed: {0:},\nhighest: {1:.2f} - {2:}'.format(kfold_relaxed,kfold_relaxed[np.argmax(kfold_relaxed)], kfold_ind[np.argmax(kfold_relaxed)])
 print 82 * '_'
 
 
@@ -72,12 +77,14 @@ svc = SVC(decision_function_shape='ovo', kernel='rbf')
 
 
 percentage = .9
-reg_acc = use_SVC_reg(X,Y,percentage,svc)
-print 'Accuracy at {0:}% Seperation: {1:.2f}\n'.format(percentage*100, reg_acc)
+Y_vald, y_hat = use_SVC_reg(X,Y,percentage,svc)
+print_performance('SVC Accuracy at {0:}% Seperation No PE'.format(percentage*100), Y_vald, y_hat)
+
 folds = 10
 print 'Using {}-Folds'.format(folds)
-kfold_acc, kfold_ind = use_kfolds(X,Y,10,svc)
+kfold_acc, kfold_relaxed,kfold_ind = use_kfolds(X,Y,folds,svc)
 print 'KFolds Accuracies: {0:},\nhighest: {1:.2f} - {2:}'.format(kfold_acc,kfold_acc[np.argmax(kfold_acc)], kfold_ind[np.argmax(kfold_acc)])
+print 'KFolds Relaxed: {0:},\nhighest: {1:.2f} - {2:}'.format(kfold_relaxed,kfold_relaxed[np.argmax(kfold_relaxed)], kfold_ind[np.argmax(kfold_relaxed)])
 print 82 * '_'
 
 
